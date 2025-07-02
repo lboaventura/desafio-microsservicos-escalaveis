ebc89d4b17aeae46db7e807a0132fdf528c1c563
lboaventura <lucasboaventurap@hotmail.com>
2025-07-02 07:52:18 -0300

    Add tracer

--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -1,3 +1,5 @@
+import "@opentelemetry/auto-instrumentations-node/register";
+
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
 import { z } from "zod";
@@ -10,6 +12,9 @@ import { db } from "../db/client.ts";
 import { schema } from "../db/schema/index.ts";
 import { randomUUID } from "crypto";
 import { dispatchOrderCreated } from "../broker/messages/order-created.ts";
+import { trace } from "@opentelemetry/api";
+import { setTimeout } from "timers/promises";
+import { tracer } from "../tracer/tracer.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -38,18 +43,27 @@ app.post(
 
     const orderId = randomUUID();
 
+    await db.insert(schema.orders).values({
+      id: orderId,
+      customerId: "6b207a15-4e25-4245-82f6-9f6a7ac03352",
+      amount,
+    });
+
+    // Testing tracing
+    const span = tracer.startSpan("code with delay");
+    span.setAttribute("test", "it's a test!");
+    await setTimeout(2000);
+    span.end();
+
+    // Testing tracing
+    trace.getActiveSpan()?.setAttribute("order_id", orderId);
+
     dispatchOrderCreated({
       orderId,
       amount,
       customer: {
         id: "6b207a15-4e25-4245-82f6-9f6a7ac03352",
       },
-    });
-
-    await db.insert(schema.orders).values({
-      id: orderId,
-      customerId: "6b207a15-4e25-4245-82f6-9f6a7ac03352",
-      amount,
     });
 
     return reply.status(201).send();
