a406e572d886bdf7f05c891e5d3b23ffb7aa2112
lboaventura <lucasboaventurap@hotmail.com>
2025-07-30 13:09:17 -0300

    Add invoices service to IaC

--- infra/index.ts
+++ infra/index.ts
@@ -1,8 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
 import { ordersService } from "./src/services/orders";
+import { invoicesService } from "./src/services/invoices";
 import { rabbitMQService } from "./src/services/rabbitmq";
 import { appLoadBalancer } from "./src/load-balancer";
 
 export const orderId = ordersService.service.id;
+export const invoicesId = invoicesService.service.id;
 export const rabbitMQId = rabbitMQService.service.id;
 export const rabbitMQAmdinUrl = pulumi.interpolate`http://${appLoadBalancer.listeners[0].endpoint.hostname}:15672`;
